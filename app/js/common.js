0df350185964f2afa0a29b461b371b367a556381
Pavel <zinovev-pasha@mail.ru>
2018-01-25 11:42:45 +0300

    Edit

--- app/js/common.js
+++ app/js/common.js
@@ -1,28 +1,14 @@
-$(function() {
+$(document).ready(function() {
+	//equalheight - одинаковая высота колонок
+	//Пример списка элементов:
+	//var eqElement = ".cat_container > div, .home_news > div"
+	var eqElement = ".itemCont";
+	equalheight(eqElement);
+	$(window).load(function(){equalheight(eqElement);}).resize(function(){equalheight(eqElement);});
 
-	//SVG Fallback
-	if(!Modernizr.svg) {
-		$("img[src*='svg']").attr("src", function() {
-			return $(this).attr("src").replace(".svg", ".png");
-		});
-	};
-
-	//E-mail Ajax Send
-	//Documentation & Example: https://github.com/agragregra/uniMail
-	$("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
-
+	//Попап менеджер FancyBox
+	//Документация: http://fancybox.net/howto
+	//<a class="fancybox"><img src="image.jpg" /></a>
+	//<a class="fancybox" data-fancybox-group="group"><img src="image.jpg" /></a>
+	// $(".fancybox").fancybox();
 });
